d4c3027911d81157b02d0e697f05ddffa8a5c627
tsai <jackalcooper@gmail.com>
2022-10-08 19:04:59 +0800

    bump beaver version

--- mix.exs
+++ mix.exs
@@ -25,7 +25,7 @@ defmodule Manx.MixProject do
   defp deps do
     [
       {:nx, "~> 0.2"},
-      {:beaver, "~> 0.2.10"},
+      {:beaver, "~> 0.2.11"},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
