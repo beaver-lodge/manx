844b0f7f6ad3ca61530a2fa54b01398b635862b6
tsai <jackalcooper@gmail.com>
2022-10-15 15:19:07 +0800

    bump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Manx.MixProject do
   def project do
     [
       app: :manx,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
